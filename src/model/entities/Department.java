b7916683f9bdbb97418bd48d4d266d39f9c599e4
Carlos Avelino <carlos.avelino2.0@gmail.com>
2020-03-21 14:08:58 -0300

    findByDepartment implementation

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -5,40 +5,33 @@ import java.io.Serializable;
 public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String name;
-	
-	
+
 	public Department() {
 	}
-
 
 	public Department(Integer id, String name) {
 		this.id = id;
 		this.name = name;
 	}
 
-
 	public Integer getId() {
 		return id;
 	}
-
 
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
 
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 
 	@Override
 	public int hashCode() {
@@ -47,7 +40,6 @@ public class Department implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -66,10 +58,8 @@ public class Department implements Serializable {
 		return true;
 	}
 
-
 	@Override
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
-
 }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -4,21 +4,19 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class Seller implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String name;
 	private String email;
 	private Date birthDate;
 	private Double baseSalary;
-	
+
 	private Department department;
-	
-	
+
 	public Seller() {
 	}
-
 
 	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
@@ -29,66 +27,53 @@ public class Seller implements Serializable {
 		this.department = department;
 	}
 
-
 	public Integer getId() {
 		return id;
 	}
-
 
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getEmail() {
 		return email;
 	}
-
 
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-
 	public Date getBirthDate() {
 		return birthDate;
 	}
-
 
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
 
-
 	public Double getBaseSalary() {
 		return baseSalary;
 	}
-
 
 	public void setBaseSalary(Double baseSalary) {
 		this.baseSalary = baseSalary;
 	}
 
-
 	public Department getDepartment() {
 		return department;
 	}
 
-
 	public void setDepartment(Department department) {
 		this.department = department;
 	}
-
 
 	@Override
 	public int hashCode() {
@@ -97,7 +82,6 @@ public class Seller implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -116,14 +100,9 @@ public class Seller implements Serializable {
 		return true;
 	}
 
-
 	@Override
 	public String toString() {
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
 				+ baseSalary + ", department=" + department + "]";
 	}
-	
-	
-	
-	
 }
